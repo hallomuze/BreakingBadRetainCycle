652935c99e84d9bf012b21d634b03778cb1534e6
artist <artist@studyGit.com>
2016-07-13 01:10:03 +0900

    init commit

--- evvRetainCycle101/ViewController.swift
+++ evvRetainCycle101/ViewController.swift
@@ -12,14 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let aPerson: Human = Human()
+        aPerson.heart = Heart(person: aPerson)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+ 
 }
+
+class Human {
+    
+    var heart : Heart?
+    
+    init( ){
+         print("human created")
+    }
+    deinit{
+        print("human released!!")
+    }
+}
+
+class Heart {
+    
+    var person : Human?
+    //weak ver person: Human?  -- 이렇게 수정 하면 메모리해지됨을 볼 수 있다
+    
+    init(person:Human){
+        self.person = person
+        
+        print("heart created")
+    }
+    deinit{
+        print("heart released")
+    }
+}
+
+
 
